3161aa3782b3be0f80c77bc3d4e306682ec786ac
Andy <atjyap@gmail.com>
2015-06-05 12:01:48 -0700

    Demo API is set up with AngularJS pulling the example API.

--- boilerplate_django_angular/demo/views.py
+++ boilerplate_django_angular/demo/views.py
@@ -9,7 +9,7 @@ def message_list(request):
     Respond with demo api message
     """
     if request.method == 'GET':
-        return Response({"message":"API call works!"}, status=status.HTTP_200_OK)
+        return Response({"message":"API call works!  Let's do this!"}, status=status.HTTP_200_OK)
 
 
 
--- boilerplate_django_angular/static/app/scripts/controllers/main.js
+++ boilerplate_django_angular/static/app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the myprojectApp
  */
 angular.module('myprojectApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+  $scope.demo = "API call did not work, did you start the server?"
+
+  $http.get('/api/demo/').success(function(data){
+    $scope.demo = data.message;
+  });
+
+
   });
